d683a165c153f9a2db0f6b74a6a2d9bd78b1f6bf
Helen Burgess <me@helenburgess.id.au>
2025-01-07 16:49:03 +0800

    troubleshooting build error, code correction

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Pages from 'vite-plugin-pages';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-	plugins: [
-		vue({
-			include: [/\.vue$/, /\.md$/],
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag === 'markdown',
-				},
-			},
-		}),
-		Pages({
-			extensions: ['vue', 'md'],
-			dirs: 'src/pages',
-			extendRoute(route) {
-				// Add meta information for tutorials section
-				if (route.path.startsWith('/tutorials/')) {
-					return {
-						...route,
-						meta: {
-							layout: 'default',
-							section: 'tutorials',
-						},
-					};
-				}
-				return route;
-			},
-			onRoutesGenerated: (routes) => {
-				return routes;
-			},
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
+	plugins: [vue()],
+	build: {
+		rollupOptions: {
+			external: ['@rollup/rollup-linux-x64-gnu'],
 		},
 	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-		},
+	optimizeDeps: {
+		exclude: ['@rollup/rollup-linux-x64-gnu'],
 	},
 });
