2d1b1a7b1e02af8ea40188161fc3795ae43a2e6e
Helen Burgess <me@helenburgess.id.au>
2025-01-09 10:28:04 +0800

    adding vendor styles to src/assets

--- vite.config.js
+++ vite.config.js
@@ -3,41 +3,23 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [
-		vue({
-			include: [/\.vue$/],
-		}),
-	],
+	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
 			'~': resolve(__dirname, 'src'),
-			bulma: 'bulma/css/bulma.min.css',
 		},
 	},
 	css: {
 		postcss: true,
 		devSourcemap: true,
-		preprocessorOptions: {
-			// Remove any sass/scss options if they exist
-		},
 	},
 	build: {
 		target: 'es2015',
 		outDir: 'dist',
 		assetsDir: 'assets',
-		minify: 'terser',
-		rollupOptions: {
-			external: [],
-			output: {
-				manualChunks: {
-					emailjs: ['@emailjs/browser'],
-					bulma: ['bulma'],
-				},
-			},
-		},
 	},
 	optimizeDeps: {
-		include: ['@emailjs/browser', 'vue-router', 'bulma'],
+		include: ['@emailjs/browser', 'vue-router'],
 	},
 });
